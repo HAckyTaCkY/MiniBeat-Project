0215405abc5e71eb065b3919a0baffb3966ae3eb
AbhishekBalija <107603928+HAckyTaCkY@users.noreply.github.com>
2023-09-05 20:21:53 +0530

    index.js

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ for (var i=0 ; i<Numberofdrums ; i++){
     document.querySelectorAll(".drum")[i].addEventListener("click",function(){
     var buttonInnerHtml = this.innerHTML;
     makeSound(buttonInnerHtml);
+    buttonAnimation(buttonInnerHtml);
 
 
 });
@@ -13,6 +14,7 @@ for (var i=0 ; i<Numberofdrums ; i++){
 
     document.addEventListener("keypress",function(event){
         makeSound(event.key)
+        buttonAnimation(event.key)
     })
 
 function makeSound(key){
@@ -55,5 +57,14 @@ function makeSound(key){
         default: console.log("buttonInnerHtml");
             break;
     }
+
+}
+
+function buttonAnimation(currentKey){
+    var activeButton = document.querySelector("." + currentKey);
+    activeButton.classList.add("pressed");
+    setTimeout(function(){
+    activeButton.classList.remove("pressed") ;
+    },100)
 
 }
